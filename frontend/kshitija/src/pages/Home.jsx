e19ccadc24b0d2bc83b743ee612405207235bc96
Sumukh Bhat <sumukhbhat.dev@gmial.com>
2026-09-23 05:04:09 +0530

    logo final 2

--- frontend/kshitija/src/pages/Home.jsx
+++ frontend/kshitija/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import nsslogo from "../assets/nsslogo.png";
 import herobg from "../assets/hero-bg.png";
 import ppclogo from "../assets/ppc-logo2.png"
-import kshitijaLogo from "../assets/kshitijaaFF.webp"
+import kshitijaLogo from "../assets/kshitijaaa finall2.png"
 function Home() {
   const [timeLeft, setTimeLeft] = useState({
     days: 0,
